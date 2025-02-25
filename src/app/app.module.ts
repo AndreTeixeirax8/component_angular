3d9dd679bf830e27263e4e46c99930d2e7937aff
André Teixeira <andre-x8@hotmail.com>
2025-02-25 14:14:45 -0300

    adiciona card button cancel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
+import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardButtonCancelComponent],
   imports: [BrowserModule, CardsModule],
   providers: [],
   bootstrap: [AppComponent],
