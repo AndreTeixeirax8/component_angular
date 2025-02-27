6326de656bc1c9378f3c847499b856c239095454
André Teixeira <andre-x8@hotmail.com>
2025-02-27 07:49:50 -0300

    feat: adiciona encapsulamento global em css

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
+  encapsulation: ViewEncapsulation.None, //remove o encapsulation com isso podemos criar classes globais no css
 })
 export class CardComponent {
   plano = {
